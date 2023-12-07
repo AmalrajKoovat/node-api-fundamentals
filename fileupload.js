8f478edf8e092e861ef626377977ab2e0116349a
AmalrajKoovat <amalrajkoovat333@gmail.com>
2023-12-07 20:05:17 +0530

    first commit

---
+++ fileupload.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+app.use(express.static('public'));
+app.get('/index.html', function (req, res) {
+    console.log("/index start")
+    res.sendFile( __dirname + "/" + "index.html" );
+    console.log("/index end")
+ })
+
+ var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+    
+    console.log("Example app listening at http://%s:%s", host, port)
+ })
