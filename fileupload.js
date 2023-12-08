574ccfe591cfa07464fecabb8df3dd2152fab9f6
AmalrajKoovat <amalrajkoovat333@gmail.com>
2023-12-08 13:18:42 +0530

    first commit

--- fileupload.js
+++ fileupload.js
@@ -1,8 +1,43 @@
 var express = require('express');
 var app = express();
+const multer = require('multer');
+const path = require('path');
 var fs = require("fs");
 
 app.use(express.static('public'));
+
+const storage = multer.diskStorage({
+   destination: function (req, file, cb) {
+     cb(null, './uploads/'); 
+   },
+   filename: function (req, file, cb) {
+     cb(null, file.originalname); 
+   }
+ });
+
+ const upload = multer({ storage: storage });
+
+ app.post('/upload', upload.single('image'), (req, res) => {
+   if (!req.file) {
+     return res.status(400).send('Please upload a file');
+   }
+   res.status(200).send('File uploaded successfully');
+ });
+
+ app.get('/download/:filename', (req, res) => {
+   const fileName = req.params.filename;
+   const filePath = path.join(__dirname, 'uploads', fileName);
+ 
+   fs.stat(filePath, (err, stats) => {
+      if (err || !stats.isFile()) {
+        return res.status(404).send('File not found');
+      }
+      res.download(filePath);
+    });
+ });
+
+ 
+
 app.get('/index.html', function (req, res) {
     console.log("/index start")
     res.sendFile( __dirname + "/" + "index.html" );
